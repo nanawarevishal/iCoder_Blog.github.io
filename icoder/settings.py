440e65801946a441c2637743fe629af084d607cb
Vishal <nanawarevishal18@gmail.com>
2022-08-21 12:15:11 +0530

    Update settings.py

--- icoder/settings.py
+++ icoder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4a&j*yisb9b%!93jk33@1qe0c^q71q^p-qj#*9tq+-x3b$)@j3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
